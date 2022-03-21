da58f27351394b2b9f04b60eda37b0178fa5dbb9
Umeigbo Obumneme Steven <obumneme.umeigbo@decagon.dev>
2022-03-21 23:41:25 +0100

    swagger configuration

--- src/main/java/com/example/walletapp/WalletAppApplication.java
+++ src/main/java/com/example/walletapp/WalletAppApplication.java
@@ -1,11 +1,13 @@
 package com.example.walletapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "Wallet API", version = "2.0", description = "Wallet transactions for users"))
 public class WalletAppApplication {
 
     public static void main(String[] args) {
